16b8390470867ddfdf16601d8bff1637e4aeb9e2
Saleem <saleemf340@gmail.com>
2023-10-17 19:55:15 +0200

    re-added handleS with correct name and betty

---
+++ handleS.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * handle_s - input string output string
+ * @args: string
+ * Return: string
+ */
+char *handle_s(va_list args)
+{
+	char *source;
+	char *final;
+	int len;
+
+	source = va_arg(args, char *);
+	if (source == NULL)
+	{
+		source = "(null)";
+	}
+	len = _strlen(source);
+	final = malloc(sizeof(char) * len + 1);
+
+	if (final == NULL)
+	{
+		return (NULL);
+	}
+		int x = 0
+
+	while (source[x] != '\0')
+	{
+		final[x] = source[x];
+		x++;
+	}
+
+	final[x] = source[x];
+
+	return (final);
+}
